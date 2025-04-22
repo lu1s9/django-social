25e65894436e8d523d3020840a7cc1b3efb49da6
Luis Fernando <sloopy.max@gmail.com>
2025-04-22 05:23:57 -0600

    Register Comment, Friendship, and FriendRequest models in the admin panel

--- website/admin.py
+++ website/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Friendship, FriendRequest
 
 admin.site.register(Post)
-
+admin.site.register(FriendRequest)
+admin.site.register(Friendship)
+admin.site.register(Comment)
